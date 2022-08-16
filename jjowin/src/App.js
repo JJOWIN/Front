b87061adaebe323129c8aa3a7d2694a127e88628
knh6269 <knh6269@naver.com>
2022-08-16 22:17:24 +0900

    08.16

--- jjowin/src/App.js
+++ jjowin/src/App.js
@@ -1,11 +1,9 @@
 import  Router  from "./Router";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   return (
     <>
     <Router/>
-    <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
--- jjowin/src/index.js
+++ jjowin/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "react-query";
 import App from './App';
 
-const queryClient = new QueryClient();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider>
       <App />
-    </QueryClientProvider>
 
   </React.StrictMode>
 );
