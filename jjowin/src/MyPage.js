cc8cbb6370a8fd1a54fa73844b462973168d17d1
qor4 <qor_4@naver.com>
2022-08-18 16:33:04 +0900

    feat: 마이페이지 프로필

--- jjowin/src/MyPage.js
+++ jjowin/src/MyPage.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {Link} from "react-router-dom"
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import profile from './images/profile.png'
 
 const Container = styled.div`
     background-color: beige;
@@ -11,6 +12,7 @@ const Container = styled.div`
     justify-content: center;
     width:50%;
     height:100vh;
+    flex-direction: column;
 `
 const Container2 = styled.div`
     border: 1px solid black;
@@ -38,10 +40,15 @@ const Span = styled.span`
 `;
 
 function MyPage(){
-
+    let data = {name : '이름', role : '직무'}
     return(
         <div style={{display:"flex"}}>
            <Container>
+            <div>
+            <img src={profile} alt='profile' style={{width:"30%"}}/>
+            <h2>{data.name}</h2>
+            <h2>{data.role}</h2>
+            </div>
              <Container2>
                 <Link to="./myPageInfoFix"><Span><h3>회원정보 수정</h3></Span></Link>
                 <Link to="./myPageSelect"><Span><h3>찜</h3></Span></Link>
