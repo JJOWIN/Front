9a741d7b95090b8d5ea732794201b71b568b1036
qor4 <qor_4@naver.com>
2022-09-10 20:02:08 +0900

    feat: 공모전 프로젝트 리스트 분야별 분류

--- jjowin/src/routes/ContestProjectList.js
+++ jjowin/src/routes/ContestProjectList.js
@@ -40,70 +40,85 @@ function ContestProjectList(){
        {
         contesttNum: 1,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅈㅇ 엑셀러레이션"
+        title: "ㅈㅇ 엑셀러레이션",
+        field:'과학'
        },
        {
         contesttNum: 2,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅋㄹㅋ 엑셀러레이션"
+        title: "ㅋㄹㅋ 엑셀러레이션",
+        field:'과학'
        },
        {
         contesttNum: 3,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㄹㅇ 엑셀러레이션"
+        title: "ㄹㅇ 엑셀러레이션",
+        field:'과학'
        },
        {
         contesttNum: 4,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "창업 엑셀러레이션"
+        title: "창업 엑셀러레이션",
+        field:'개발'
        },
        {
         contesttNum: 5,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "창업 엑셀러레이션"
+        title: "창업 엑셀러레이션",
+        field:'개발'
        },
        {
         contesttNum: 6,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "창업 엑셀러레이션"
+        title: "창업 엑셀러레이션",
+        field:'개발'
        },
        {
         contesttNum: 7,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅎㅎ 엑셀러레이션"
+        title: "ㅎㅎ 엑셀러레이션",
+        field:'개발'
        },
        {
         contesttNum: 8,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅋㅋ 엑셀러레이션"
+        title: "ㅋㅋ 엑셀러레이션",
+        field:'개발'
        },
        {
         contesttNum: 9,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅎㅎ 엑셀러레이션"
+        title: "ㅎㅎ 엑셀러레이션",
+        field:'디자인'
        },
        {
         contesttNum: 10,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㅈㅈ 엑셀러레이션"
+        title: "ㅈㅈ 엑셀러레이션",
+        field:'디자인'
        },
        {
         contesttNum: 11,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㄷㄷ 엑셀러레이션"
+        title: "ㄷㄷ 엑셀러레이션",
+        field:'디자인'
        },
        {
         contesttNum: 12,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㄷㄷ 엑셀러레이션"
+        title: "ㄷㄷ 엑셀러레이션",
+        field:'디자인'
        },
        {
         contesttNum: 13,
         src: "https://image.msscdn.net/mfile_s01/_shopstaff/list.staff_62d74cf0e1bf5.jpg",
-        title: "ㄷㄷ 엑셀러레이션"
+        title: "ㄷㄷ 엑셀러레이션",
+        field:'디자인'
        },
     ]
     const [key,SetKey] = useState("");
+    const [contestList,setContestList] = useState(contest);
+    const [fieldValue, setFieldValue] =useState(false);
    
     let search=contest.filter((item)=>{
         return(item.title.includes(key));
@@ -112,7 +127,40 @@ function ContestProjectList(){
     const Change = (e) =>{
         SetKey(e.target.value);
     };
-    
+    const onClickSearch=()=>{
+        console.log(fieldValue)
+        if(fieldValue){
+            let tmp = contest.filter((e)=>e.field === fieldValue)
+            console.log(tmp)
+            setContestList(tmp)
+        }
+        console.log(contestList)
+
+    }
+    const OPTIONS = [
+        { value: false, name:'선택'},
+        { value: "개발", name: "개발" },
+        { value: "디자인", name: "디자인" },
+        { value: "과학", name: "과학" },
+    ];
+    const SelectBox = (props) => {
+        const handleChange = (e) => {
+            setFieldValue(e.target.value);
+        };
+        return (
+            <select onChange={handleChange} value={fieldValue}>
+                {props.options.map((option) => (
+                    <option
+                        key={option.value}
+                        value={option.value}
+                        // defaultValue={props.defaultValue === option.value}
+                    >
+                        {option.name}
+                    </option>
+                ))}
+            </select>  
+        );
+    };
 
     return(
         <>
@@ -122,10 +170,14 @@ function ContestProjectList(){
         <div style={{display:"flex",marginLeft:"7vw",marginTop:"3vh"}}>
         <h5>검색창:</h5>
         <Input onChange={Change}></Input>
+        <h5>분야</h5>
+        <SelectBox options={OPTIONS} defaultValue={false}></SelectBox>
+        <button onClick={onClickSearch}>검색</button>
+
         </div>
         <div style={{display:"flex",flexWrap:"wrap",justifyContent:"center",textAlign:"center",paddingRight:"4vw"}}>
         {
-        search.map((item)=>{
+        contestList.map((item)=>{
             const url =`/contestList/contestProjectDetail/${item.contesttNum}`;
             return(
                 <Collection>
